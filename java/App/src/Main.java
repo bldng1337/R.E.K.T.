fe33ff38a3c0e928c15fb2df2dabe507b5c4791b
bldng1337 <50496940+bldng1337@users.noreply.github.com>
2022-10-21 09:56:48 +0200

    Support for mutiple Lines

--- java/App/src/Main.java
+++ java/App/src/Main.java
@@ -2,8 +2,11 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import imgui.ImFontConfig;
 import imgui.ImFontGlyphRangesBuilder;
@@ -18,16 +21,33 @@ import imgui.flag.ImGuiConfigFlags;
 import imgui.type.ImBoolean;
 
 public class Main extends Application {
-	public static List<Float> data=new LinkedList<>();
+	class Datapoint{
+		private final float data;
+		private final long timestamp;
+		public Datapoint(float data,long timestamp) {
+			this.data=data;
+			this.timestamp=timestamp;
+		}
+		public float getData() {
+			return data;
+		}
+		public long getTimestamp() {
+			return timestamp;
+		}
+	}
+	public static Map<String, List<Datapoint>> data=new HashMap<>();
 	
     @Override
     protected void configure(Configuration config) {
-        config.setTitle("Dear ImGui is Awesome!");
-       
-        for(int i=0;i<300;i++)
-        	data.add((float)Math.random());
-       String FileName = "C:\\Users\\BF\\Desktop\\t.csv";
-       ExtractorUtils.writeCVS(ExtractorUtils.convertList(data), FileName);
+	    config.setTitle("Dear ImGui is Awesome!");
+	    data.put("Temperatur", new LinkedList<>());
+	    data.put("Feuchtigkeit", new LinkedList<>());
+	    
+//	    String FileName = "C:\\Users\\BF\\Desktop\\t.csv";
+//		ExtractorUtils.writeCVS(ExtractorUtils.convertList(data), FileName);
+	  //Start Demo Data
+	    for(int i=0;i<1000;i++)
+	    	data.forEach((a,b)->b.add(new Datapoint((float) (Math.random()*50f),b.size())));
     }
   
     
@@ -72,16 +92,29 @@ public class Main extends Application {
     public void process() {
     	ImGui.dockSpaceOverViewport();
     	//ImGui.showStyleEditor();
-    	if(data.size()>100000)
-    		data.remove(0);
-    	data.add((float)Math.random());
+    	data.forEach((a,b)->{
+    		if(b.size()>10_000)
+    			b.remove(0);
+    	});
+    	data.forEach((a,b)->b.add(new Datapoint((float) (Math.random()*50f),b.size())));
     	
     	
     	if (ImGui.begin("Plot")) {
     		if(ImGui.button("AutoScroll"))
         		autoscroll=!autoscroll;
 	        if (ImPlot.beginPlot("Plot","x","y",ImGui.getContentRegionAvail(),ImPlotFlags.None , ImPlotAxisFlags.None|(autoscroll?ImPlotAxisFlags.AutoFit:ImPlotAxisFlags.None), ImPlotAxisFlags.AutoFit)) {
-	            ImPlot.plotLine("Line", autoscroll?getxs(200):getxs(data.size()), autoscroll?data.subList(data.size()-200, data.size()).toArray(new Float[200]):data.toArray(new Float[data.size()]));
+	            
+	        	data.forEach((a,b)->{
+	        		List<Float> vals=b.stream().map((data)->data.getData()).collect(Collectors.toList());
+	        		List<Float> time=b.stream().map((data)->(float)data.getTimestamp()).collect(Collectors.toList());
+	        		
+	        		
+	        		ImPlot.plotLine(a, 
+	        			(autoscroll?time.subList(Math.max(time.size()-100, 0), time.size()):time).toArray(new Float[1]),
+	        			(autoscroll?vals.subList(Math.max(vals.size()-100, 0), vals.size()):vals).toArray(new Float[1]));
+	        	});
+	        	
+	            
 	            ImPlot.endPlot();
 	        }
     	}
